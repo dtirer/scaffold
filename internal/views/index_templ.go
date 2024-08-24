cd741a15c7aa36f06c9d4fe59e0aca33286859e8
Dan Tirer <dan@stationhead.com>
2024-08-24 12:38:38 -0400

    added HTMX

--- internal/views/index_templ.go
+++ internal/views/index_templ.go
@@ -23,7 +23,7 @@ func Index() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link href=\"/public/css/output.css\" rel=\"stylesheet\"><title>Document</title></head><body><h1 class=\"text-4xl\">Hello, World! Check this out can't touch this</h1></body></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link href=\"/public/css/output.css\" rel=\"stylesheet\"><script src=\"/public/js/htmx.min.js\"></script><title>Document</title></head><body><h1 class=\"text-4xl\">Hello, World! Check this out can't touch this</h1></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
