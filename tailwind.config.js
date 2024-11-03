f389261739f25a79810371f31383ec0f152bd25e
Dan Tirer <dan@stationhead.com>
2024-11-02 23:18:55 -0400

    adding support for custom fonts, with Inter being the default

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./pkg/views/**/*.{html,js,templ}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
